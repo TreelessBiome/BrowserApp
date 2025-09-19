8779ddcc10dbf2876855b5353107b9e2ff4f8b61
TreelessBiome <advaitmore2009@gmail.com>
2025-09-19 12:58:26 -0600

    defines URL and its constructor. Adds method to GET data from a server.

---
+++ main.py
@@ -0,0 +1,31 @@
+import socket
+
+class URL:
+    #Class Constructor
+    def __init__(self, url):
+        self.scheme, url = url.split('://');
+
+        assert self.scheme == "http", "scheme has to be http"
+
+        if "/" not in url:
+            url = url + "/"
+
+        self.host, url = url.split('/', 1);
+        self.path = url + "/";
+
+    def request(self):
+        s = socket.socket(
+            family = socket.AF_INET,
+            type = socket.SOCK_STREAM,
+            proto = socket.IPPROTO_TCP
+        );
+        s.connect((self.host, 80))
+
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n"
+        s.send(request.encode("utf-8"))
+
+        response = s.makefile("r", encoding="utf-8", newline="\r\n")
+
+
